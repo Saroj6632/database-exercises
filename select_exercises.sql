8913c13fdaa80a8d90eb14da72ecb84f496ac97a
Saroj Duwal <saroj.duwal@gmail.com>
2022-05-18 18:21:20 -0400

    made some changes to solutions

--- select_exercises.sql
+++ select_exercises.sql
@@ -11,6 +11,8 @@ select count(distinct(artist)) from albums;
 
 select release_date from albums
 order by release_date;
+select min(release_date) from albums;
+select max(release_date) from albums;
 -- 3d.What is the oldest release date for any album in the albums table? What is the most recent release date?
 -- ans 3d.oldest 1967 and most recent= 2011
 
@@ -24,16 +26,16 @@ select name from albums where artist='Pink Floyd';
 
 
 -- b. The year Sgt. Pepper's Lonely Hearts Club Band was released
-select release_date from albums where artist='Sgt.Pepper' & name='Lonely Hearts Club Band';
+select release_date from albums where name ="Sgt. Pepper's Lonely Hearts Club Band";
 
 -- c. The genre for the album Nevermind
 select genre from albums where name='Nevermind';
 
 -- d. Which albums were released in the 1990s
-select name from albums where release_date='1990';
+select name from albums where release_date between '1990' and '1999';
 
 -- e. Which albums had less than 20 million certified sales
-select name from albums  where sales< '20 million';
+select name from albums  where sales< 20 ;
 
 
 -- f. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
@@ -41,3 +43,6 @@ select name from albums where genre='Rock';
 -- it doesnot include Hard rock or progressive rock as they are treated different genre.
 -- to show up all the rock including progressive and hard
 select name from albums where genre like'%rock%';
+
+
+
