58fd3f9ed6fe228dbf3bdc02a46c49d982e27746
Saroj Duwal <saroj.duwal@gmail.com>
2022-05-19 18:57:30 -0400

    updated solutions functions_exercises.sql

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,6 +1,16 @@
+#1.
 use employees;
+
+#2.
+
+#3.
 select first_name, last_name, concat(first_name,last_name)as full_name from employees  where last_name like 'E%E';
+
+
+#4.
 select first_name, last_name, upper(concat(first_name,last_name))as full_name from employees  where last_name like 'E%E';
+
+#5.
 SELECT 
     first_name, last_name, birth_date, hire_date, datediff(curdate(),hire_date) as number_of_days
 FROM
@@ -9,4 +19,17 @@ WHERE
     birth_date LIKE '1%-12-25'
         AND (hire_date BETWEEN '1900-1-1' AND '1999-12-31');
 select min(salary), max(salary) from salaries;
+
+#6.
+SELECT 
+    first_name,
+    last_name,
+    lower(CONCAT(SUBSTR(last_name, 1, 1),
+                    substr(first_name, 1, 4),
+                    '_',
+                    MONTH(birth_date),
+                    right(YEAR('birth_date',2)))) as user_name
+FROM
+    employees; 
+
 
