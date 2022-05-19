eca427496668fdf9e9390c94164099d6f9cdd075
Saroj Duwal <saroj.duwal@gmail.com>
2022-05-19 14:59:05 -0400

    answer to question 4

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -10,9 +10,12 @@ order by hire_date asc
 limit 5;
 
 #4.
+
+
 select* from employees where birth_date like'19%-12-25' and (hire_date between '1990-01-01' and '1999-12-31')
 order by hire_date asc
-limit 5 offset
+limit 5 offset 45;
+
 
 
 
